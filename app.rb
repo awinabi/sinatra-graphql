22b714431f730796d760e5b2c7245d1e027b088e
Awin Abi <awin@awinawin.awin>
2018-02-24 00:06:33 +0530

    Sample json hello request/response

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
+require "sinatra/json"
 
 class ConferenceApp < Sinatra::Base
 
   get '/' do
     'It Works!'
+  end
+
+  get '/hello.json' do
+    message = { success: true, message: 'hello'}
+    json message
   end
 end
