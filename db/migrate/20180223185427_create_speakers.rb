6d9293d866297e6e3b8e0b853e06c6139ebb02d5
Awin Abi <awin@awinawin.awin>
2018-02-24 00:27:48 +0530

    Speaker fields and run migrations

--- db/migrate/20180223185427_create_speakers.rb
+++ db/migrate/20180223185427_create_speakers.rb
@@ -1,4 +1,10 @@
 class CreateSpeakers < ActiveRecord::Migration[5.1]
   def change
+    create_table :speakers do |t|
+      t.string :name, null: false
+      t.string :twitter_handle
+      t.text :bio
+      t.string :talk_title
+    end
   end
 end
