b78b6aff57c76d8a368e16a0ee22cbfede856a2e
Awin Abi <awin@awinawin.awin>
2018-02-24 23:35:17 +0530

    Add /graphql POST endpoint
    * This accepts a json payload with query and variables and executes the
    * graphql schema to render json results

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
+require 'pry'
 require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/activerecord'
+require 'rack/contrib'
 
 require_relative 'models/speaker'
+require_relative 'graphql/schema'
 
 class ConferenceApp < Sinatra::Base
   set :database_file, 'config/database.yml'
+  use Rack::PostBodyContentTypeParser
 
   get '/' do
     'It Works!'
@@ -19,5 +23,14 @@ class ConferenceApp < Sinatra::Base
   get '/speakers' do
     @speakers = Speaker.all
     json @speakers
+  end
+
+  post '/graphql' do
+    result = ConferenceAppSchema.execute(
+      params[:query],
+      variables: params[:variables],
+      context: { current_user: nil },
+    )
+    json result
   end
 end
