2606909fb152767f9c59723638d05fd8b496f875
Awin Abi <awin@awinawin.awin>
2018-12-19 12:38:20 +0530

    Creating a mutation root and add it to schema

---
+++ graphql/mutation.rb
@@ -0,0 +1,5 @@
+require 'graphql'
+
+MutationType = GraphQL::ObjectType.define do
+  name 'Mutation'
+end
--- graphql/schema.rb
+++ graphql/schema.rb
@@ -1,6 +1,8 @@
 require 'graphql'
 require_relative 'query'
+require_relative 'mutation'
 
 ConferenceAppSchema = GraphQL::Schema.define do
   query QueryType
+  mutation MutationType
 end
